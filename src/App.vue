6ead550b720932a2acabc3edb7989cf3baf9c33c
Андрей Деревянкин <a_derevyankin@MacBook-Air-Andrej.local>
2023-01-10 18:52:32 +0300

    purge

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,12 @@ module.exports = {
         'red': ' #f31b31',
         'blue': ' #2a72f8',
         'purple': '#722ba1',
+        'dark': '#1b1b1b',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
-      },  },
+      },
     },
+  },
   plugins: [],
 }
