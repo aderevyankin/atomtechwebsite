7ad3807992b385f17d821b306ee0077b90a5040d
Andrey Derevyankin <a.derevyankin@atomtech.dev>
2023-01-18 18:05:51 +0300

    разработан сайдбар

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+var sideMenu = document.querySelector('menu');
+
+document.getElementById('open-menu').addEventListener('click', function() {
+    sideMenu.classList.add('side-menu-open');
+    sideMenu.classList.remove('side-menu-close');
+})
+
+document.getElementById('close-menu').addEventListener('click', function () {
+    sideMenu.classList.add('side-menu-close');
+    sideMenu.classList.remove('side-menu-open');
+})
