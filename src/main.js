47a82c74d26e3f92356227f34da77a2e1a3a3fdf
Andrey.Derevyankin <a.derevyankin@atomtech.dev>
2023-01-25 21:30:45 +0300

    карусель

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './style.css'
+import 'flowbite';
 
 var sideMenu = document.querySelector('menu');
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}', './src/**/*.{vue,js,ts,jsx,tsx,css}'],
+  content: ['./*.{html,js}', './src/**/*.{vue,js,ts,jsx,tsx,css}',"./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
@@ -30,5 +30,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
