d05596ccd01c3b6f8a2f54a69401ba3b6d20b400
Andrey Derevyankin <a.derevyankin@atomtech.dev>
2023-02-10 17:46:22 +0300

    спиннер на кнопке Отправить и несколько замечаний

--- src/main.js
+++ src/main.js
@@ -1,80 +1,95 @@
-import 'flowbite';
-import { Modal } from 'flowbite';
-import './style.css';
-import './counter.js'
+import "flowbite";
+import { Modal } from "flowbite";
+import "./style.css";
+import "./counter.js";
 const URI_API = `https://functions.yandexcloud.net/d4etsced6ta9eilot2po`;
 
+document
+    .getElementById("img_highQuality")
+    .addEventListener("load", function (event) {
+        var target = document.getElementById("bgblock");
+        target.style.backgroundImage = "url(" + event.target.src + ")";
+    });
 
-document.getElementById('img_highQuality').addEventListener('load', function (event) {
-    var target = document.getElementById("bgblock");
-    target.style.backgroundImage = "url(" + event.target.src + ")";
+var sideMenu = document.querySelector("menu");
+
+document.getElementById("open-menu").addEventListener("click", function () {
+    sideMenu.classList.add("side-menu-open");
+    sideMenu.classList.remove("side-menu-close");
 });
 
-var sideMenu = document.querySelector('menu');
-
-document.getElementById('open-menu').addEventListener('click', function () {
-    sideMenu.classList.add('side-menu-open');
-    sideMenu.classList.remove('side-menu-close');
-})
-
-document.getElementById('close-menu').addEventListener('click', function () {
-    sideMenu.classList.add('side-menu-close');
-    sideMenu.classList.remove('side-menu-open');
-})
+document.getElementById("close-menu").addEventListener("click", function () {
+    sideMenu.classList.add("side-menu-close");
+    sideMenu.classList.remove("side-menu-open");
+});
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
-document.getElementById("next-button").addEventListener('click', function nextSlide() {
-    showSlides(slideIndex += 1);
-});
+document
+    .getElementById("next-button")
+    .addEventListener("click", function nextSlide() {
+        showSlides((slideIndex += 1));
+    });
 
-document.getElementById("prev-button").addEventListener('click', function previousSlide() {
-    showSlides(slideIndex -= 1);
-});
+document
+    .getElementById("prev-button")
+    .addEventListener("click", function previousSlide() {
+        showSlides((slideIndex -= 1));
+    });
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("section");
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
     for (let slide of slides) {
         slide.style.display = "none";
     }
-    slides[slideIndex - 1].style.display = window.innerWidth >= 768 ? "flex" : "block";
+    slides[slideIndex - 1].style.display =
+        window.innerWidth >= 768 ? "flex" : "block";
 }
 
+const buttonLoading = document.getElementById('form-button-loading');
+buttonLoading.style.display = 'none';
 
-const $targetEl = document.getElementById('result-modal');
+const $targetEl = document.getElementById("result-modal");
 const modal = new Modal($targetEl);
-const modаlText = document.getElementById('result-text');
-let resultMessage = '';
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault(); //сбросил стандартное поведение
+const modаlText = document.getElementById("result-text");
+let resultMessage = "";
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+        e.preventDefault(); //сбросил стандартное поведение
+        buttonLoading.style.display = 'block';
+        let msg = `<b>Заявка с сайта</b>\n`;
+        msg += `<b>Имя:</b> ${this.name.value}\n`;
+        msg += `<b>Компания:</b> ${this.company.value}\n`;
+        msg += `<b>Почта:</b> ${this.email.value}\n`;
+        msg += `<b>Телефонный номер:</b> ${this.phonenum.value}\n`;
 
-    let msg = `<b>Заявка с сайта</b>\n`;
-    msg += `<b>Имя:</b> ${this.name.value}\n`;
-    msg += `<b>Компания:</b> ${this.company.value}\n`;
-    msg += `<b>Почта:</b> ${this.email.value}\n`;
-    msg += `<b>Телефонный номер:</b> ${this.phonenum.value}\n`;
-
-    msg = encodeURIComponent(msg);
-    fetch(`${URI_API}?text=${msg}`)
-        .then(res => {
-            if (!res.ok) throw 'сетевая ошибка';
-            this.name.value = '';
-            this.company.value = '';
-            this.email.value = '';
-            this.phonenum.value = '';
-            resultMessage = 'Заявка отправлена. В ближайшее время менеджер с вами свяжется';
-        }).catch((err) => {
-            console.log(err);
-            resultMessage = 'Не удалось отправить заявку. Попробуйте позже.';
-        }).finally(() => {
-            modаlText.innerHTML = resultMessage;
-            modal.show();
-        })
-})
+        msg = encodeURIComponent(msg);
+        fetch(`${URI_API}?text=${msg}`)
+            .then((res) => {
+                if (!res.ok) throw "сетевая ошибка";
+                this.name.value = "";
+                this.company.value = "";
+                this.email.value = "";
+                this.phonenum.value = "";
+                resultMessage =
+                    "Заявка отправлена. В ближайшее время менеджер с вами свяжется";
+            })
+            .catch((err) => {
+                console.log(err);
+                resultMessage = "Не удалось отправить заявку. Попробуйте позже.";
+            })
+            .finally(() => {
+                modаlText.innerHTML = resultMessage;
+                modal.show();
+                setTimeout(() => {
+                    modal.hide();
+                    buttonLoading.style.display = 'none';
+                }, 3500);
+            });
+    });
